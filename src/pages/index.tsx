411e1abd9daeeff82aaace625bc04c3f61ad433f
Fabricyo Barreto <bass.barreto@gmail.com>
2021-05-15 12:47:51 -0300

    Page: Foi feito a estrutura/estilização da pagina Home/index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,36 @@
+import Head from 'next/head';
+
+import styles from './home.module.scss';
+
 export default function Home(): JSX.Element {
   return (
-    <h1>Olá mundo</h1>
+    <>
+      <Head>
+        <title>Home | gi.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>
+            👏 Hey, welcome
+          </span>
+          <h1>
+            News about the
+            {' '}
+            <span>React</span>
+            {' '}
+            world
+          </h1>
+          <p>
+            Get access to all the publications
+            {' '}
+            <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
   );
 }
