4fb48467a6c9dfa826e5a38a60d78fc51cf73a89
Fabricyo Barreto <bass.barreto@gmail.com>
2021-05-28 16:51:46 -0300

    Post: Foi estruturado/estilizado a pagina que mostra o post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable import/no-extraneous-dependencies */
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
@@ -28,11 +30,13 @@ export default function Posts({ posts }: PostsProps): JSX.Element {
         <div className={styles.posts}>
           {
             posts.map((post) => (
-              <a key={post.slug} href="##">
-                <time>{post.updatedAt}</time>
-                <strong>{post.title}</strong>
-                <p>{post.excerpt}</p>
-              </a>
+              <Link href={`/posts/${post.slug}`}>
+                <a key={post.slug}>
+                  <time>{post.updatedAt}</time>
+                  <strong>{post.title}</strong>
+                  <p>{post.excerpt}</p>
+                </a>
+              </Link>
             ))
           }
         </div>
